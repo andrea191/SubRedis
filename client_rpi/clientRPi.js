4e237b4fb601be0f29cdfe0c8df17871e539652b
andrea191 <andrea.m.91@gmail.com>
2016-06-29 13:56:11 +0300

    rpi client with temperature sensor added

--- client_rpi/clientRPi.js
+++ client_rpi/clientRPi.js
@@ -1,5 +1,17 @@
 var sensor = require('ds18x20');
 
 sensor.isDriverLoaded(function(err, isLoaded){
+	if (err) {
+		console.log('somenthing went wrong loading the driver:', err);
+	};
 	console.log(isLoaded);
+});
+
+sensor.list(function(err, listOfDeviceIds){
+	console.log(listOfDeviceIds);
+});
+
+
+sensor.getAll(function(err, tempObj){
+	console.log(tempObj);
 });
